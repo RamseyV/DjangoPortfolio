e5c691a38201e67c336c0bad866a3531018cb71d
Ramsey <ramseyvillarreal@gmail.com>
2019-03-18 16:32:18 -0400

    Projects Page Created
    Created Projects Page. Referenced in views  and urls. Created Cards for Projects 
    TODO: choose better colors for projects page
    Also: adjusted some navbar styling and deleted models for now

--- admin.py
+++ admin.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
-from .models import Subjects, Languages, WCUClasses
+# from .models import Subjects, Languages, WCUClasses
 
-class WCUClassesAdmin(admin.ModelAdmin):
-	 fieldsets = [
-        ("Class Name", {'fields': ["name"]}),
-        ("Description", {'fields': ["description"]}),
-        ("Subject", {'fields': ["subject"]}),
-        ("Languages", {"fields": ["languages"]})
-    ]
+# class WCUClassesAdmin(admin.ModelAdmin):
+# 	 fieldsets = [
+#         ("Class Name", {'fields': ["name"]}),
+#         ("Description", {'fields': ["description"]}),
+#         ("Subject", {'fields': ["subject"]}),
+#         ("Languages", {"fields": ["languages"]})
+#     ]
 
 
-admin.site.register(Subjects)
-admin.site.register(Languages)
-admin.site.register(WCUClasses,WCUClassesAdmin)
+# admin.site.register(Subjects)
+# admin.site.register(Languages)
+# admin.site.register(WCUClasses,WCUClassesAdmin)
 # admin.site.register(SkillPlanets)
